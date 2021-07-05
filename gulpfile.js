5699a197203e3419a63a86c4dc85b2260bb4a102
Jorge Lopez <jlopezg112@miumg.edu.gt>
2021-07-04 21:54:42 -0600

    gulp-notify for gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-const { series, src, dest, watch } = require('gulp');
+const { series, src, dest, watch, parallel } = require('gulp');
 
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
 
 
 //funcion que compila SASS
@@ -11,20 +12,27 @@ function css(){
             outputStyle: 'expanded'
         }))
         .pipe(dest("./build/css"))
+        .pipe(notify({message: 'CSS Compilado.'}))
 }
 
+/* Funcion que sirve para comprimir el peso
+del archivo app.css , eliminando los espacios innecesarios*/
 function minificarCSS(){
     return src("src/scss/layout/app.scss")
         .pipe(sass({
             outputStyle : 'compressed'
         }))
         .pipe(dest("./build/css"))
+        .pipe(notify({message: 'CSS Minificado/Comprimido'}))
 }
 
+//Reduce significativamente el peso de las imagenes, guarda el archivo
+//Comprimido el buil/img
 function imagenes(){
     return src('src/img/**/*')
         .pipe(imagemin())
         .pipe(dest('./build/img'))
+        .pipe(notify({message: 'Imagen Minificada'}));
 }
 
 
@@ -39,3 +47,5 @@ exports.css = css;
 exports.minificarCSS = minificarCSS;
 exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
+
+exports.default = series(css, imagenes, watchArchivos);
